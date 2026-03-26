cd8da43cab1e0902c76780fd918b80e34d7882d8
Anna Kharitonchik <zharitonchik@icloud.com>
2026-03-26 23:46:34 +0300

    implementation toggleAll, clearCompleted, countByStatus, crud

--- JS-TS/solutions/todo-bulk.ts
+++ JS-TS/solutions/todo-bulk.ts
@@ -1,13 +1,30 @@
-import { Todo, TodoStatus } from './types';
+import {Todo, TodoStatus} from './types';
 
 export function toggleAll(state: Todo[], completed: boolean): Todo[] {
-  throw new Error('toggleAll: not implemented');
+    const stateCopy: Todo[] = JSON.parse(JSON.stringify(state))
+    stateCopy.map((item) => {
+        if (completed) {
+            item.status = TodoStatus.COMPLETED
+            return item
+
+        } else {
+            item.status = TodoStatus.IN_PROGRESS
+            return item
+        }
+    })
+    return stateCopy
 }
 
 export function clearCompleted(state: Todo[]): Todo[] {
-  throw new Error('clearCompleted: not implemented');
+    return state.filter((item) => item.status !== TodoStatus.COMPLETED)
+
 }
 
 export function countByStatus(state: Todo[], status: TodoStatus): number {
-  throw new Error('countByStatus: not implemented');
+    return state.reduce((acc, item) => {
+        if (item.status === status) {
+            acc++;
+        }
+        return acc
+    }, 0)
 }
--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -1,17 +1,34 @@
-import { Todo } from './types';
+import {Todo} from './types';
 
 export function addTodo(state: Todo[], todo: Todo): Todo[] {
-  throw new Error('addTodo: not implemented');
+    const stateCopy: Todo[] = JSON.parse(JSON.stringify(state));
+    stateCopy.push(todo)
+    return stateCopy;
 }
 
 export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
-  throw new Error('updateTodo: not implemented');
+
+    const stateCopy: Todo[] = JSON.parse(JSON.stringify(state));
+    const todoArr = stateCopy.filter((el) => el.id === id);
+    if (!todoArr) {
+        throw new Error("todo not found")
+    }
+    const todo = todoArr[0];
+    todo.title = update.title;
+    todo.description = update.description;
+    todo.status = update.status;
+    return stateCopy;
 }
 
 export function removeTodo(state: Todo[], id: number): Todo[] {
-  throw new Error('removeTodo: not implemented');
+    const stateCopy: Todo[] = JSON.parse(JSON.stringify(state));
+    const filteredArr = stateCopy.filter((el) => el.id !== id);
+    if (filteredArr.length === stateCopy.length) {
+        throw new Error("todo not found")
+    }
+    return filteredArr;
 }
 
 export function getTodo(state: Todo[], id: number): Todo | undefined {
-  throw new Error('getTodo: not implemented');
+    return state.filter((el) => el.id === id)[0];
 }
